8fc64e597711a7ab91923692ec9e4fe503643d45
Ashutosh Bhardwaj <ashutosh24135@iiitd.ac.in>
2024-12-12 22:03:31 +0000

    Added Basic Framework of the Code
    -> Basic Ray Tracing
    -> Mirrors
    -> Objects

---
+++ 2DLightSim/game.py
@@ -0,0 +1,41 @@
+import pygame
+import numpy as np
+import math
+from lights import Lights
+from objects import Object
+from mirror import Mirror
+
+pygame.init()
+pygame.display.set_caption("Light")
+
+width = 720
+height = 720
+screen = pygame.display.set_mode((width, height))
+clock, fps = pygame.time.Clock(), 1000
+surface = pygame.Surface((720, 720), pygame.SRCALPHA)
+surface.fill((0, 0, 0, 0))
+light = Lights(400, 300, 240, 500)
+light2 = Lights(500, 400, 240, 500)
+brightness = np.zeros((width, height), dtype=np.float32)
+mirror1 = Mirror(200, 250, 75, surface)
+objects = [(300, 400, 20)]
+mirrors = [mirror1]
+running = True
+while running:
+
+    brightness.fill(0)
+    
+    for obj in objects:
+        pygame.draw.circle(surface, (255, 0, 0), (obj[0], obj[1]), obj[2])
+
+    light.lighting(screen, brightness, objects, mirrors)
+    screen.blit(surface, (0, 0))
+    pygame.display.flip()
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+       
+
+pygame.quit()
---
+++ 2DLightSim/lights.py
@@ -0,0 +1,60 @@
+import pygame 
+import math
+import numpy 
+
+
+pygame.init()
+
+class Lights:
+    def __init__(self, x, y, color, intensity):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.intensity = intensity
+
+    def lighting(self, screen, brightness, objects, mirrors):
+        for i in range(720):
+            angle = 2 * math.pi * i / 360
+            rayx = self.x
+            rayy = self.y 
+
+            raydx = math.cos(angle)
+            raydy = math.sin(angle)
+
+            ray_length = 0
+
+            while ray_length < 800:
+                rayx += raydx
+                rayy += raydy 
+
+                for obj in objects:
+                    if math.sqrt((rayx - obj[0])**2 + (rayy - obj[1])**2) <= obj[2]:
+                        ray_length = 1000
+                        break
+
+                for obj in mirrors:
+                    if 0 <= abs(rayx  - obj.x) <= 1 and obj.y - obj.length/2 <= int(rayy) <= obj.y + obj.length/2:
+                        angle = obj.reflect(self, angle)
+                        raydx = math.cos(angle)
+                        raydy = math.sin(angle)
+
+                if (int(rayx) >= 720 or int(rayx) < 0 or int(rayy) >= 720 or int(rayy) < 0 ):
+                    break
+                
+                brightness[int(rayx)][int(rayy)] += (self.intensity/(ray_length + 0.0001))
+                intense = min(255, int(brightness[int(rayx)][int(rayy)])*10)
+                screen.set_at((int(rayx), int(rayy)), (intense, intense, intense))
+
+                ray_length += 1
+
+                
+                
+
+        
+
+    
+
+
+        
+        
+        
